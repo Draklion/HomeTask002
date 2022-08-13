be23851e791ac0fb5ff656db3ef98a2aff0d76e5
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 15:41:47 +0500

    HomeTask002.8 completed.

--- HomeTasks002.8/Program.cs
+++ HomeTasks002.8/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 8. Программа, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.");
+
+  Console.Write("Введите первое число: ");
+  int firstNumber = Int32.Parse(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  int secondNumber = Int32.Parse(Console.ReadLine());
+
+  Console.WriteLine(firstNumber == secondNumber * secondNumber || secondNumber == firstNumber * firstNumber ? "Да" : "Нет");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
