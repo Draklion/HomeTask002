ba176f3cc804736a7866f1c310a4d8b21a6c41b9
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 15:27:50 +0500

    HomeTask002.6 completed.

--- HomeTasks002.6/Program.cs
+++ HomeTasks002.6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 6. Программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.");
+
+  Console.Write("Введите число: ");
+  int number = Int32.Parse(Console.ReadLine());
+
+  Console.WriteLine(number % 7 == 0 && number % 23 == 0 ? "Да" : "Нет");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
