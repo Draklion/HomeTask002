77d4f426d3016eebfad2c56313e2a48cc635b35e
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 15:38:19 +0500

    HomeTask002.7 completed.

--- HomeTasks002.7/Program.cs
+++ HomeTasks002.7/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 7. Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+  Console.Write("Введите число дня недели: ");
+  int number = Int32.Parse(Console.ReadLine());
+
+  Console.WriteLine(number <= 7 && number >= 1 ? ((number == 1 || number == 2 || number == 3 || number == 4 || number == 5) ? "Нет" : "Да") : "Дней недели всего 7!");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
