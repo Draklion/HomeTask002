1e7402eb03418606125b99edee66dd055410a806
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 12:44:15 +0500

    HomeTask002.3 completed.

--- HomeTasks002.3/Program.cs
+++ HomeTasks002.3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 3. Программа, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.");
+
+  Console.Write("Введите трехзначное число: ");
+  string numberstr = Console.ReadLine();
+
+  Console.WriteLine($"Число без второй цифры: {numberstr[0]}{numberstr[numberstr.Length - 1]}");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
