6459ded2e3f2250d36fe8bb5d43f193599b4a5a3
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 13:46:31 +0500

    HomeTask002.5 completed.

--- HomeTasks002.5/Program.cs
+++ HomeTasks002.5/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 5. Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+  Console.Write("Введите любое число: ");
+  string numberstr = Console.ReadLine();
+
+  Console.WriteLine(numberstr.Length <= 2 ? $"Третьей цифры нет" : $"Третья цифра числа: {numberstr[2]}");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
