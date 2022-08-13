67ab76282da776c33f5e2d52b858a792c7d6eebf
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 13:31:15 +0500

    HomeTask002.4 completed.

--- HomeTasks002.4/Program.cs
+++ HomeTasks002.4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 4. Программа, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.");
+
+  Console.Write("Введите первое число: ");
+  int firstNumber = Int32.Parse(Console.ReadLine());
+  Console.Write("Введите второе число: ");
+  int secondNumber = Int32.Parse(Console.ReadLine());
+
+  Console.WriteLine(firstNumber % secondNumber == 0 ? "Кратно" : $"Не кратно: {firstNumber % secondNumber}");
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
