582773e61fd19cfb5dc6b41185d7e585bd4fbd7b
«Dmitry_Machnev» <draklion56@gmail.com>
2022-08-13 12:25:58 +0500

    HomeTask002.1 completed.

--- HomeTasks002.1/Program.cs
+++ HomeTasks002.1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+  Console.WriteLine("Задание 1. Программа, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.");
+  int calculator1(int number)
+  {
+    string numberstr = number.ToString();
+    number = Int32.Parse(numberstr[numberstr.Length - 1].ToString()) > Int32.Parse(numberstr[0].ToString()) ?
+    Int32.Parse(numberstr[numberstr.Length - 1].ToString()) : Int32.Parse(numberstr[0].ToString());
+
+    return number;
+  }
+
+  int calculator2(int number)
+  {
+    number = number / 10 > number % 10 ? number / 10 : number % 10;
+    return number;
+  }
+
+  var number = new Random().Next(10, 100);
+  Console.WriteLine($"Случайное число: {number}");
+  Console.WriteLine($"Решение через строку: {calculator1(number)}");
+  Console.WriteLine($"Решение через вычисление: {calculator2(number)}");
+
+}
+catch
+{
+  Console.WriteLine("Вы ввели некорректное значение.");
+}
